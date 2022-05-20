ec5b8c06678257d26ad73b072945e55f6f408b3e
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-05-20 17:23:24 -0300

    cabeçalho para todos forms

---
+++ frontend/header.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Restaurante</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="index.html">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="cardapios.php">Cardápio</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="itens.php">Itens</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="ingredientes.php">Ingredientes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="nutricionistas.php">Nutricionistas</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="usuarios.php">Usuários</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+            </form>
+        </div>
+    </div>
+</nav>
