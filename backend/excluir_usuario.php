ffd421d2620b6d9ed3bcd65589db7e569a3631f6
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-05-31 09:32:19 -0300

    Add files via upload

---
+++ backend/excluir_usuario.php
@@ -0,0 +1,17 @@
+<?php
+
+    include_once 'usuario.class.php';
+
+    if(isset($_POST['id'])){
+
+        $usuario = new cardapio();
+
+            $usuario->set_id($_POST['id']);
+
+        $usuario->excluir();
+
+    }
+
+    header("location: ../frontend/usuarios.php");
+
+?>
