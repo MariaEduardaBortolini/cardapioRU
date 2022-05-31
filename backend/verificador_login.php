517cccea6329459b1833bc540bb6d759fd4c50e3
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-05-31 09:36:13 -0300

    Update verificador_login.php

--- backend/verificador_login.php
+++ backend/verificador_login.php
@@ -1,22 +1,7 @@
 <?php
 
     include_once 'login.class.php';
-
-    // verifica se ja existe o login e, se não existe, redireciona para a página de login
-    // deve ser colado no topo das páginas "proibidas"
-
-    if($_SESSION['logado'] !== true){
-
-        // altere para o nome do arquivo onde sera feito o login
-        header('login.php');
-
-    }else{
-
-        // carrega o resto da pagina aqui
-
-    }
     
-    // parte para o arquivo no qual sera enviado o formulário com os dados para login
     if(isset($_POST['email']) && isset($_POST['senha'])){
 
         $login = new login();
@@ -26,12 +11,11 @@
 
         if($login->verificar()){
 
-            header('index.php');
+            header('location: ../frontend/ingredientes.php');
 
         }else{
 
-            // volta para o login caso tenha algo errado
-            header('login.php');
+            header('location: login.php');
 
         }
 
