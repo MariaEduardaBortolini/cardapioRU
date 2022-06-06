a7c9a1bb528ec6453377aea89d06ccc426bb04a9
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-06-06 14:41:23 -0300

    Update excluir_ingrediente.php

--- backend/excluir_ingrediente.php
+++ backend/excluir_ingrediente.php
@@ -2,11 +2,11 @@
 
     include_once 'cardapio.class.php';
 
-    if(isset($_GET['id'])){
+    if(isset($_POST['id'])){
 
         $ingrediente = new cardapio();
 
-            $ingrediente->set_ingr_id($_GET['id']);
+            $ingrediente->set_ingr_id($_POST['id']);
 
         $ingrediente->excluir_ingr();
 
