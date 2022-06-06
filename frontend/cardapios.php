7e745279b52c000cb81d629f7c6787d8a8c0a851
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-06-06 14:48:24 -0300

    Update cardapios.php

--- frontend/cardapios.php
+++ frontend/cardapios.php
@@ -35,9 +35,11 @@
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Cardápios</h1>
                 <div class="pull-right"> 
-                    <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Novo
-                    </button>
+                    <a href="cadastro_cardapios.php">
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                            Novo
+                        </button>
+                    </a>
                 </div>
             </div>			
 
@@ -156,7 +158,12 @@
                                 ?>
                             </td>
                             <td><?php echo $soma_calorias; ?></td>
-                            <td><a href="../backend/excluir_cardapio.php?id=<?php echo $cardapio['id'] ?>"><button class="btn btn-danger" data-bs-dismiss="modal">Excluir</button></a></td>
+                            <td>
+                                <form method="POST" action="../backend/excluir_cardapio.php">
+                                    <input type="hidden" name="id" value="<?php echo $cardapio['id']; ?>">
+                                    <button type="submit" class="btn btn-danger">Excluir</button>
+                                </form>
+                            </td>
                         </tr>
                         <?php
                             }
@@ -169,117 +176,11 @@
                 <div class="d-flex justify-content-between py-4 my-4 border-top">
                     <p>&copy; 2022 Company, Inc. All rights reserved.</p>
                 </div>
-            </footer>						
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Cardápio</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                            <form method="POST" action="../backend/salvar_cardapio.php">
-                                <div class="modal-body">
-                                    <div class="mb-3">
-                                        <label for="tipo" class="form-label">Tipo</label>
-                                        <select class="form-control" id="tipo" name="tipo">
-                                            <option value="cafe">Café da Manhã</option>
-                                            <option value="almoco">Almoço</option>
-                                            <option value="janta">Janta</option>
-                                        </select>
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="data" class="form-label">Data</label>
-                                        <input type="date" name="dia">
-                                    </div>
-                                    <div class="mb-3">
-                                        <label for="nutri" class="form-label">Nutricionista</label>
-                                        <select class="form-control" id="nutri" name="nutri">
-                                            <option value="0" selected>Selecione uma nutricionista</option>
-                                            <?php
-
-                                                foreach($nutris as $nutri){
-
-                                            ?>
-                                                <option value="<?php echo $nutri['id']; ?>"><?php echo $nutri['nome']; ?></option>
-                                            <?php
-
-                                                }
-
-                                            ?>
-                                        </select>
-                                    </div>
-                                    <div class="mb-3" id="select_item">
-                                        <label for="itens" class="form-label">Itens</label>
-                                        <button class="btn" id="mais">Adiconar Mais</button>
-                                        <select class="form-control itens" id="item0">
-                                            <option value="0" selected>Selecione um item</option>
-                                            <?php
-
-                                                $infos->set_item_id('');
-
-                                                foreach($itens as $item){
-
-                                            ?>
-                                                <option value="<?php echo $item['id']; ?>"><?php echo $item['nome']; ?></option>
-                                            <?php
-
-                                                }
-
-                                            ?>
-                                        </select>
-                                    </div>
-                                    <input type="hidden" name="item" id="item">
-                                    <div class="modal-footer">
-                                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                                        <button id="salvar" type="submit" class="btn btn-success">Salvar</button>
-                                    </div>
-                                </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </footer>
 
         </main>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="scripts.js"></script>
-        <script>
-            var btn = document.getElementById('mais');
-            let item = document.getElementById('item0');
-            var c = 1;
-            btn.onclick = function(){
-
-                let clonedItem = item.cloneNode(true);
-                clonedItem.id = 'item' + c;
-
-                document.getElementById('select_item').appendChild(clonedItem);
-
-                c++;
-
-                return false;
-
-            }
-
-            var btn_s = document.getElementById('salvar');
-            btn_s.onclick = function(){
-
-                var item_array = document.getElementsByClassName('itens');
-
-                var novo_json = {
-                    
-                }
-                
-                for (let i = 0; i < item_array.length; i++) {
-                    if(item_array[i].value != 0){
-                        novo_json[i] = item_array[i].value;
-                    }
-                }
-
-                document.getElementById('item').value = JSON.stringify(novo_json);
-
-            }
-        </script>
     </body>
 </html>
