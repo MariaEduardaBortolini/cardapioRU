075ce033e399fee91229697f6d188ccf76bfabd5
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-06-06 14:49:37 -0300

    Update itens.php

--- frontend/itens.php
+++ frontend/itens.php
@@ -35,9 +35,11 @@
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">Itens</h1>
                 <div class="pull-right"> 
-                    <button type="submit" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                        Novo
-                    </button>
+                    <a href="cadastro_itens.php">
+                        <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                            Novo
+                        </button>
+                    </a>
                 </div>
             </div>			
 
@@ -106,7 +108,12 @@
                                 ?>
                             </td>
                             <td><?php echo $soma_calorias; ?></td>
-                            <td><a href="../backend/excluir_item.php?id=<?php echo $item['id'] ?>"><button class="btn btn-danger" data-bs-dismiss="modal">Excluir</button></a></td>
+                            <td>
+                                <form method="POST" action="../backend/excluir_item.php">
+                                    <input type="hidden" name="id" value="<?php echo $item['id']; ?>">
+                                    <button type="submit" class="btn btn-danger">Excluir</button>
+                                </form>
+                            </td>
                         </tr>
                         
                         <?php
@@ -121,94 +128,12 @@
                 <div class="d-flex justify-content-between py-4 my-4 border-top">
                     <p>&copy; 2022 Company, Inc. All rights reserved.</p>
                 </div>
-            </footer>						
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Itens</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-                            <form method="POST" action="../backend/salvar_item.php">
-                                <div class="mb-3">
-                                    <label for="nome" class="form-label">Nome</label>
-                                    <input type="text" class="form-control" id="nome" name="nome" placeholder="Informe o nome">
-                                </div>
-                                <div class="mb-3">
-                                    <label for="descr" class="form-label">Descrição</label>
-                                    <input type="text" class="form-control" id="descr" name="descr" placeholder="Informe a descrição">
-                                </div>
-                                <div class="mb-3" id="select_ingr"> 
-                                    <label for="ingrs" class="form-label">Ingredientes</label>
-                                    <button class="btn" id="mais">Adiconar Mais</button>
-                                    <select class="form-control ingrs" id="ingr0">
-                                        <option value="0" selected>Selecione um ingrediente</option>
-                                        <?php
-
-                                            foreach($ingredientes as $ingrediente){
-
-                                        ?>
-                                            <option value="<?php echo $ingrediente['id']; ?>"><?php echo $ingrediente['nome']; ?></option>
-                                        <?php
-
-                                            }
-
-                                        ?>
-                                    </select>
-                                </div> 
-                                <input type="hidden" name="ingr" id="ingr">
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cancelar</button>
-                                    <button id="salvar" type="submit" class="btn btn-success">Salvar</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            </footer>
 
         </main>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="scripts.js"></script>
-        <script>
-            var btn = document.getElementById('mais');
-            let ingr = document.getElementById('ingr0');
-            var c = 1;
-            btn.onclick = function(){
-
-                let clonedIngr = ingr.cloneNode(true);
-                clonedIngr.id = 'ingr' + c;
-
-                document.getElementById('select_ingr').appendChild(clonedIngr);
-
-                c++;
-
-                return false;
-
-            }
-
-            var btn_s = document.getElementById('salvar');
-            btn_s.onclick = function(){
-
-                var ingr_array = document.getElementsByClassName('ingrs');
-
-                var novo_json = {
-                    
-                }
-                
-                for (let i = 0; i < ingr_array.length; i++) {
-                    if(ingr_array[i].value != 0){
-                        novo_json[i] = ingr_array[i].value;
-                    }
-                }
-
-                document.getElementById('ingr').value = JSON.stringify(novo_json);
-
-            }
-        </script>
+        
     </body>
 </html>
