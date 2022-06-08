66607d0d59ab4c89dc258ef29d455109f0afc824
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-06-08 08:26:03 -0300

    Update index.php

--- frontend/index.php
+++ frontend/index.php
@@ -12,11 +12,25 @@
      </head> 
   
      <body>
-       <?php
-       
-         include_once 'header.php';
-       
-       ?>
+       <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Restaurante</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="login.php">Login</a>
+                </li>
+            </ul>
+            <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Pesquisar">
+                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+            </form>
+                </div>
+            </div>
+        </nav>
       <main class="container"> 
         <div class="bg-light p-5 rounded"> 
           <h1>Restaurante</h1> 
